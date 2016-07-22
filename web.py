5d4706e3916ae0a1ec7fd8414f7a79a7a0b472f4
liangchaob <liangchaob@163.com>
2016-07-22 11:21:49 +0800

    test

--- web.py
+++ web.py
@@ -12,8 +12,15 @@ sys.path.append("./")
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
-# 配置类
+import time  
+from flask import Flask,request, make_response  
+import hashlib  
+# 微信配置类
 from wechat_sdk import WechatConf
+# 微信接口类
+from wechat_sdk import WechatBasic
+
+# 配置参数
 conf = WechatConf(
     token='g7824tgfhew0g', 
     appid='wxa9312a82e8138370', 
@@ -21,9 +28,40 @@ conf = WechatConf(
     # encrypt_mode='safe',  # 可选项：normal/compatible/safe，分别对应于 明文/兼容/安全 模式
     # encoding_aes_key='your_encoding_aes_key'  # 如果传入此值则必须保证同时传入 token, appid
 )
-
-# 接口类
-from wechat_sdk import WechatBasic
 wechat = WechatBasic(conf=conf)
+
+
+# flask实例化
+app = Flask(__name__)
+
+
+# 微信验证
+@app.route('/wechat_auth', methods = ['GET', 'POST'])
+def wechat_auth():
+    if request.method == 'GET':
+        # 参数接收
+        query = request.args 
+        signature = query.get('signature', '')  
+        timestamp = query.get('timestamp', '')  
+        nonce = query.get('nonce', '')  
+        echostr = query.get('echostr', '')
+        # 验证
+        if wechat.check_signature(signature, timestamp, nonce):
+            print 'Accept'
+            return echostr
+        else:
+            print 'Wrong'
+
+
+
+
+
+# 运行主函数
+if __name__ == '__main__':
+    # 对外开放80端口
+    app.run(host='0.0.0.0',port=80,debug=True)
+
+
+
 
 
