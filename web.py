91576a1b728a9d66c67ec484e7d7ec91e4bb6fae
liangchaob <liangchaob@163.com>
2016-07-22 16:52:47 +0800

    10条

--- web.py
+++ web.py
@@ -141,26 +141,6 @@ def wechat_auth():
                     'description': u'第三条新闻描述',
                     'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
                     'url': u'http://www.v2ex.com/',
-                },{
-                    'title': u'第11条新闻标题',
-                    'description': u'第三条新闻描述',
-                    'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
-                    'url': u'http://www.v2ex.com/',
-                },{
-                    'title': u'第12条新闻标题',
-                    'description': u'第三条新闻描述',
-                    'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
-                    'url': u'http://www.v2ex.com/',
-                },{
-                    'title': u'第13条新闻标题',
-                    'description': u'第三条新闻描述',
-                    'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
-                    'url': u'http://www.v2ex.com/',
-                },{
-                    'title': u'第14条新闻标题',
-                    'description': u'第三条新闻描述',
-                    'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
-                    'url': u'http://www.v2ex.com/',
                 }
             ])
             return feedback
