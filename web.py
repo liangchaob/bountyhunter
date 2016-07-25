8d141329a3f66b0bf7a3d987516e61e21857abef
liangchaob <liangchaob@163.com>
2016-07-25 13:44:11 +0800

    测试菜单

--- web.py
+++ web.py
@@ -22,7 +22,10 @@ from wechat_sdk import WechatBasic
 # 微信xml解析类
 from wechat_sdk.exceptions import ParseError
 
-
+import urllib
+import urllib2
+from urllib import urlencode
+import json
 
 
 
@@ -37,6 +40,34 @@ conf = WechatConf(
     # encoding_aes_key='your_encoding_aes_key'  # 如果传入此值则必须保证同时传入 token, appid
 )
 wechat = WechatBasic(conf=conf)
+
+
+# 菜单设置
+menu_data = {
+    "button": [
+        {
+            "type": "click", 
+            "name": "发任务", 
+            "url": "www.baidu.com"
+        }, 
+        {
+            "type": "click", 
+            "name": "接任务", 
+            "url": "www.baidu.com"
+        }
+    ]
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 # flask实例化
@@ -56,6 +87,8 @@ def wechat_auth():
         # 验证
         if wechat.check_signature(signature, timestamp, nonce):
             print 'Accept'
+            # 建立菜单
+            wechat.create_menu(menu_data)
             return echostr
         else:
             print 'Wrong'
@@ -72,78 +105,101 @@ def wechat_auth():
             rec_time = wechat.message.time      # 对应于 XML 中的 CreateTime
             rec_type = wechat.message.type      # 对应于 XML 中的 MsgType
             # rec_raw = wechat.message.raw        # 原始 XML 文本，方便进行其他分析
-            rec_content = wechat.message.content # post的文本信息
+            # rec_content = wechat.message.content # post的文本信息
             print "消息id:"+str(rec_id)
             print "公众号id:"+str(rec_target)
             print "用户openid:"+str(rec_source)
             print "时间戳:"+str(rec_time)
             print "消息类型:"+str(rec_type)
             # print rec_raw
-            print "信息内容:"+str(rec_content)
+            # print "信息内容:"+str(rec_content)
 
-            # feedback = wechat.response_text(content=
-            #     "消息id:"+str(rec_id)+
-            #     "\n公众号id:"+str(rec_target)+
-            #     "\n用户openid:"+str(rec_source)+
-            #     "\n时间戳:"+str(rec_time)+
-            #     "\n消息类型:"+str(rec_type)+
-            #     "\n信息内容:"+str(rec_content)
-            #     )
-            # return feedback
+            if rec_type == 'subscribe':  # 关注事件(包括普通关注事件和扫描二维码造成的关注事件)
+                key = wechat.message.key                        # 对应于 XML 中的 EventKey (普通关注事件时此值为 None)
+                ticket = wechat.message.ticket                  # 对应于 XML 中的 Ticket (普通关注事件时此值为 None)
+            
+            elif rec_type == 'text':
+                rec_content = wechat.message.content
+                print "消息id:"+str(rec_id)
+                print "公众号id:"+str(rec_target)
+                print "用户openid:"+str(rec_source)
+                print "时间戳:"+str(rec_time)
+                print "消息类型:"+str(rec_type)
+                feedback = wechat.response_news([
+                    {
+                        'title': u'第1条新闻标题',
+                        'picurl': u'http://o7m541j22.bkt.clouddn.com/biznetwork.png',
+                        'description': u'第一条新闻描述，这条新闻没有预览图',
+                        'url': u'http://www.baidu.com/',
+                    }, {
+                        'title': u'第2条新闻标题, 这条新闻无描述',
+                        'picurl': u'http://o7m541j22.bkt.clouddn.com/biznetwork.png',
+                        'url': u'http://www.github.com/',
+                    }, {
+                        'title': u'第3条新闻标题',
+                        'description': u'第三条新闻描述',
+                        'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
+                        'url': u'http://www.v2ex.com/',
+                    },{
+                        'title': u'第4条新闻标题',
+                        'description': u'第三条新闻描述',
+                        'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
+                        'url': u'http://www.v2ex.com/',
+                    },{
+                        'title': u'第5条新闻标题',
+                        'description': u'第三条新闻描述',
+                        'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
+                        'url': u'http://www.v2ex.com/',
+                    },{
+                        'title': u'第6条新闻标题',
+                        'description': u'第三条新闻描述',
+                        'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
+                        'url': u'http://www.v2ex.com/',
+                    },{
+                        'title': u'第7条新闻标题',
+                        'description': u'第三条新闻描述',
+                        'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
+                        'url': u'http://www.v2ex.com/',
+                    },{
+                        'title': u'第8条新闻标题',
+                        'description': u'第三条新闻描述',
+                        'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
+                        'url': u'http://www.v2ex.com/',
+                    },{
+                        'title': u'第9条新闻标题',
+                        'description': u'第三条新闻描述',
+                        'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
+                        'url': u'http://www.v2ex.com/',
+                    },{
+                        'title': u'第10条新闻标题',
+                        'description': u'第三条新闻描述',
+                        'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
+                        'url': u'http://www.v2ex.com/',
+                    }
+                ])
+                return feedback
 
-            feedback = wechat.response_news([
-                {
-                    'title': u'第1条新闻标题',
-                    'picurl': u'http://o7m541j22.bkt.clouddn.com/biznetwork.png',
-                    'description': u'第一条新闻描述，这条新闻没有预览图',
-                    'url': u'http://www.baidu.com/',
-                }, {
-                    'title': u'第2条新闻标题, 这条新闻无描述',
-                    'picurl': u'http://o7m541j22.bkt.clouddn.com/biznetwork.png',
-                    'url': u'http://www.github.com/',
-                }, {
-                    'title': u'第3条新闻标题',
-                    'description': u'第三条新闻描述',
-                    'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
-                    'url': u'http://www.v2ex.com/',
-                },{
-                    'title': u'第4条新闻标题',
-                    'description': u'第三条新闻描述',
-                    'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
-                    'url': u'http://www.v2ex.com/',
-                },{
-                    'title': u'第5条新闻标题',
-                    'description': u'第三条新闻描述',
-                    'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
-                    'url': u'http://www.v2ex.com/',
-                },{
-                    'title': u'第6条新闻标题',
-                    'description': u'第三条新闻描述',
-                    'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
-                    'url': u'http://www.v2ex.com/',
-                },{
-                    'title': u'第7条新闻标题',
-                    'description': u'第三条新闻描述',
-                    'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
-                    'url': u'http://www.v2ex.com/',
-                },{
-                    'title': u'第8条新闻标题',
-                    'description': u'第三条新闻描述',
-                    'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
-                    'url': u'http://www.v2ex.com/',
-                },{
-                    'title': u'第9条新闻标题',
-                    'description': u'第三条新闻描述',
-                    'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
-                    'url': u'http://www.v2ex.com/',
-                },{
-                    'title': u'第10条新闻标题',
-                    'description': u'第三条新闻描述',
-                    'picurl': u'http://http://o7m541j22.bkt.clouddn.com/group.png',
-                    'url': u'http://www.v2ex.com/',
-                }
-            ])
-            return feedback
+
+            # elif wechat.message.type == 'unsubscribe':  # 取消关注事件（无可用私有信息）
+            #     pass
+            # elif wechat.message.type == 'scan':  # 用户已关注时的二维码扫描事件
+            #     key = wechat.message.key                        # 对应于 XML 中的 EventKey
+            #     ticket = wechat.message.ticket                  # 对应于 XML 中的 Ticket
+            # elif wechat.message.type == 'location':  # 上报地理位置事件
+            #     latitude = wechat.message.latitude              # 对应于 XML 中的 Latitude
+            #     longitude = wechat.message.longitude            # 对应于 XML 中的 Longitude
+            #     precision = wechat.message.precision            # 对应于 XML 中的 Precision
+            # elif wechat.message.type == 'click':  # 自定义菜单点击事件
+            #     key = wechat.message.key                        # 对应于 XML 中的 EventKey
+            # elif wechat.message.type == 'view':  # 自定义菜单跳转链接事件
+            #     key = wechat.message.key                        # 对应于 XML 中的 EventKey
+            # elif wechat.message.type == 'templatesendjobfinish':  # 模板消息事件
+            #     status = wechat.message.status                  # 对应于 XML 中的 Status
+            # elif wechat.message.type in ['scancode_push', 'scancode_waitmsg', 'pic_sysphoto', 
+            #                              'pic_photo_or_album', 'pic_weixin', 'location_select']:  # 其他事件
+            #     key = wechat.message.key                        # 对应于 XML 中的 EventKey
+
+            
 
         except ParseError:
             print 'Invalid Body Text'
@@ -151,7 +207,9 @@ def wechat_auth():
 
 
 
+# 菜单函数
 
+    # 菜单设置
 
 
 
@@ -161,6 +219,21 @@ def wechat_auth():
 if __name__ == '__main__':
     # 对外开放80端口
     app.run(host='0.0.0.0',port=80,debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
