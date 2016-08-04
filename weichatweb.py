14784483de920e5d80bd3ad6af1ee8bc46f73b49
liangchaob <liangchaob@163.com>
2016-08-04 14:43:08 +0800

    sae add

--- weichatweb.py
+++ weichatweb.py
@@ -234,7 +234,7 @@ def new_mission():
         # s = s.decode('utf-8')
         # return str(s)
         # return jsonify(req_userinfo.json())
-        return render_template('t1.html',nickname = s,url_openid = url_openid,url_userinfo = url_userinfo,
+        return render_template('t1.html',nickname = nickname,url_openid = url_openid,url_userinfo = url_userinfo,
             sex = sex, province = province, city=city,country = country,headimgurl= headimgurl)
         # return url_userinfo
 
