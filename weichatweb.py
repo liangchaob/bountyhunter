f488b9ef05bc74f5eeb1f8d73ea67a15d5ee58ef
liangchaob <liangchaob@163.com>
2016-08-11 16:22:51 +0800

    sae add

--- weichatweb.py
+++ weichatweb.py
@@ -241,7 +241,7 @@ def new_mission():
                 'acceptor':13
                 }
             # r = requests.post('http://liangchaob-bountyhunter.daoapp.io/mission/', data=json.dumps(jsonobj))
-            return str(jsonobj.get('openid'))+str(jsonobj.get('name'))
+            return json.dumps(jsonobj)
         except:
             return 'wrong!'
     else:
