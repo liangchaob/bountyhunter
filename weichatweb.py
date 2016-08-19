c7d41dfab4ed8353337bacb901958568f872f770
liangchaob <liangchaob@163.com>
2016-08-19 12:26:29 +0800

    sae add

--- weichatweb.py
+++ weichatweb.py
@@ -465,10 +465,7 @@ def missionApproval(mission_id):
         r = requests.get('http://liangchaob-bountyhunter.daoapp.io/mission/'+str(mission_id),headers = headers)
         result = r.json()
 
-        return render_template('mission_approval.html',mission_name = result.name,
-            mission_type = result.mission_type,deadline = result.deadline,description = result.description,
-            obj = result.obj,skill_need = result.skill_need,bounty = result.bounty,state = result.state,
-            publisher = publisher)
+        return render_template('mission_approval.html',mission_approval = result)
     else:
         pass
 
