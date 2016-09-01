9227e6e586a651c7f7fb11a9ea179cd332459838
liangchaob <liangchaob@163.com>
2016-09-01 18:56:37 +0800

    sae add

--- static/js/example.js
+++ static/js/example.js
@@ -21,8 +21,8 @@ $(function () {
         url: '/mission_name',
         className: 'mission_name',
         render: function () {
-            return '<h1>mission_name</h1>';
-            // return $('#mission_name').html();
+            // return '<h1>mission_name</h1>';
+            return $('#mission_name').html();
         }
     };
 
