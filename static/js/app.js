90e76b24fd40a5d17600961a9083213b6fc80965
liangchaob <liangchaob@163.com>
2016-09-02 15:26:20 +0800

    sae add

--- static/js/app.js
+++ static/js/app.js
@@ -1,64 +1,97 @@
-angular.module('newMissionApp', ['ngAnimate', 'ngRoute'])
+angular.module('weuiapp', ['ngRoute'])
     .config(function($routeProvider) {
+        //路由配置
         $routeProvider
             .when('/', {
-                controller: 'home',
-                templateUrl: ''
+                controller: 'homeCtrl',
+                templateUrl: 'home.html'
             })
-            .when('/mission_name',{
-                controller: 'missionNameController',
-                templateUrl: 'newmission_name.html'
+            .when('/button', {
+                controller: 'buttonCtrl',
+                templateUrl: 'button.html'
             })
-            .when('/mission_name',{template:'这是电脑分类页面'})
+            .when('/cell', {
+                controller: 'cellCtrl',
+                templateUrl: 'cell.html'
+            })
+            .when('/toast', {
+                controller: 'toastCtrl',
+                templateUrl: 'toast.html'
+            })
+            .when('/dialog', {
+                controller: 'dialogCtrl',
+                templateUrl: 'dialog.html'
+            })
+            .when('/progress', {
+                controller: 'progressCtrl',
+                templateUrl: 'progress.html'
+            })
+            .when('/msg', {
+                controller: 'msgCtrl',
+                templateUrl: 'msg.html'
+            })
+            .when('/article', {
+                controller: 'articleCtrl',
+                templateUrl: 'article.html'
+            })
+            .when('/actionsheet', {
+                controller: 'actionsheetCtrl',
+                templateUrl: 'actionsheet.html'
+            })
+            .when('/icons', {
+                controller: 'iconsCtrl',
+                templateUrl: 'icons.html'
+            })
+            .when('/panel', {
+                controller: 'panelCtrl',
+                templateUrl: 'panel.html'
+            })
+            .when('/tab', {
+                controller: 'tabCtrl',
+                templateUrl: 'tab.html'
+            })
+            .when('/navbar', {
+                controller: 'navbarCtrl',
+                templateUrl: 'navbar.html'
+            })
+            .when('/tabbar', {
+                controller: 'tabbarCtrl',
+                templateUrl: 'tabbar.html'
+            })
+            .when('/searchbar', {
+                controller: 'searchbarCtrl',
+                templateUrl: 'searchbar.html'
+            })
             .otherwise({
                 redirectTo: '/'
-            })
+            });
+
 
     })
-    .controller('home', function($scope) {
-        $scope.homeShow = true;
-        $scope.viewShow = false;
+    .controller('homeCtrl', function($scope) {
+        //初始化ngView的样式为不可见
+        $scope.viewStyle = {
+            left: '200%',
+            height: 0,
+            width: 0
+        };
 
         $scope.showBlock = function() {
-            $scope.homeShow = false;
-            $scope.viewShow = true;
+            //设置ngView的样式可见
+            $scope.viewStyle = {
+                left: 0,
+                height: '100%',
+                width: '100%'
+            };
         }
     })
-    // .controller('toast', ['$scope', '$interval', toast])
-    // .animation('.aweui-show', ['$animateCss', toastAnimate])
-    // .animation('.home', ['$animateCss', function($animateCss) {
-    //     return {
-    //         enter: function(element, doneFn) {
-    //             return $animateCss(element, {
-    //                 from: { left: '100%', top: 0, opacity: 0 },
-    //                 to: { left: 0, top: 0, opacity: 1 },
-    //                 duration: .3
-    //             });
-    //         },
-    //         leave: function(element, doneFn) {
-    //             return $animateCss(element, {
-    //                 from: { left: 0, top: 0, opacity: 1 },
-    //                 to: { left: '-100%', top: 0, opacity: 0 },
-    //                 duration: .3
-    //             });
-    //         }
-    //     }
-    // }])
-    // .animation('.view', ['$animateCss', function($animateCss) {
-    //     return {
-    //         enter: function(element, doneFn) {
-    //             return $animateCss(element, {
-    //                 from: { left: '100%', top: 0, opacity: 0 },
-    //                 to: { left: 0, top: 0, opacity: 1 },
-    //                 duration: .3
-    //             });
-    //         },
-    //         leave: function(element, doneFn) {
-    //             return $animateCss(element, {
-    //                 from: { left: 0, top: 0, opacity: 1 },
-    //                 to: { left: '-100%', top: 0, opacity: 0 },
-    //                 duration: .3
-    //             });
-    //         }
-    //     }
-    // }])
+    // //增加toast控件控制器
+    // .controller('toastCtrl', ['$scope', '$interval', toastCtrl])
+    // //增加dialog控件控制器
+    // .controller('dialogCtrl', ['$scope', dialogCtrl])
+    // //增加progress控件控制器
+    // .controller('progressCtrl', ['$scope', progressCtrl])
+    // //增加actionsheet控件控制器
+    // .controller('actionsheetCtrl', ['$scope', actionsheetCtrl])
+    // //增加searchbar控件控制器
+    // .controller('searchbarCtrl', ['$scope', searchbarCtrl]);
