7156514be9337638996f2bea9aaddf5ad4ff9cef
liangchaob <liangchaob@163.com>
2016-09-02 15:10:22 +0800

    sae add

--- static/js/app.js
+++ static/js/app.js
@@ -5,11 +5,11 @@ angular.module('newMissionApp', ['ngAnimate', 'ngRoute'])
                 controller: 'home',
                 templateUrl: ''
             })
-            .when('/mission_name',{
-                // controller: 'missionNameController',
-                templateUrl: 'newmission_name.html'
-            })
-            // .when('/computers',{template:'这是电脑分类页面'})
+            // .when('/mission_name',{
+            //     // controller: 'missionNameController',
+            //     templateUrl: 'newmission_name.html'
+            // })
+            .when('/mission_name',{template:'这是电脑分类页面'})
             .otherwise({
                 redirectTo: '/'
             })
