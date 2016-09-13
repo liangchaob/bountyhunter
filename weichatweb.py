73f5a409335bdbefdd50d96a6bbf96def918c458
liangchaob <liangchaob@163.com>
2016-09-14 00:31:41 +0800

    sae add

--- weichatweb.py
+++ weichatweb.py
@@ -514,7 +514,7 @@ def comment():
             mission_id = request.form['mission_id']
 
             result = db_obj.dbpost('api/comment/',jsonobj)
-            return redirect('/wechat/mission/' + mission_id)
+            return redirect(url_for('mission',mission_id=mission_id))
 
         except Exception, e:
             pass
