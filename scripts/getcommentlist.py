56003dde5600cce8297fafcc0f1b973a7455e0cb
liangchaob <liangchaob@163.com>
2016-09-18 20:50:16 +0800

    sae add

---
+++ scripts/getcommentlist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding:utf-8
+'''
+* 赏金猎人项目
+* liangchaob@163.com 
+* 2016.7.21
+'''
+
+#设置中文字符
+import sys
+sys.path.append("./")
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
+
+
+
+import requests
+
+
+
+
+r=requests.post('http://0.0.0.0:8080/api/user/',data=payload)
+
+
+print r.text
